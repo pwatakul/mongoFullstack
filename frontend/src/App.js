f2c425f6f591378e8d9a47b69a7f690d3e402ed7
patawatw <patawat.w@gother.com>
2024-10-13 18:41:09 +0700

    fix router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import HotelList from './components/HotelList';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HotelDetail from './components/HotelDetail';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import HotelDetail from './components/HotelDetail';
 
 
 function App() {
@@ -10,63 +10,50 @@ function App() {
   const [selectedNumber, setSelectedNumber] = React.useState('');
 
   return (
-    <Router>
-      <div className="App">
-        
-        <Routes>
-          <Route 
-            path="/" 
-            element={
-              <div>
-                <div className="search-bar">
-                  <label>
-                    Search location:
-                    <input 
-                      type="text" 
-                      placeholder="Search location" 
-                      value={searchLocation}
-                      onChange={(e) => setSearchLocation(e.target.value)}
-                    />
-                  </label>
-                  <label>
-                    Select type:
-                    <select 
-                      value={selectedType}
-                      onChange={(e) => setSelectedType(e.target.value)}
-                    >
-                      <option value="">Select type</option>
-                      <option value="Apartment">Apartment</option>
-                      <option value="House">House</option>
-                      <option value="glasshouse">glasshouse</option>
-                      {/* Add more options as needed */}
-                    </select>
-                  </label>
-                  <label>
-                    Select number:
-                    <select 
-                      value={selectedNumber}
-                      onChange={(e) => setSelectedNumber(e.target.value)}
-                    >
-                      <option value="">Select number</option>
-                      {[...Array(10).keys()].map(num => (
-                        <option key={num + 1} value={num + 1}>{num + 1}</option>
-                      ))}
-                    </select>
-                  </label>
-                </div>
-                <HotelList 
-                  searchLocation={searchLocation} 
-                  locationType={selectedType} 
-                  bedroomNumber={selectedNumber} 
-                />
-              </div>
-              
-            } 
+    <div className="App">
+      <div className="search-bar">
+        <label>
+          Search location:
+          <input 
+            type="text" 
+            placeholder="Search location" 
+            value={searchLocation}
+            onChange={(e) => setSearchLocation(e.target.value)}
           />
-          <Route path="/hotel/:hotelId" element={<HotelDetail />} />
-        </Routes>
+        </label>
+        <label>
+          Select type:
+          <select 
+            value={selectedType}
+            onChange={(e) => setSelectedType(e.target.value)}
+          >
+            <option value="">Select type</option>
+            <option value="Apartment">Apartment</option>
+            <option value="House">House</option>
+            <option value="glasshouse">glasshouse</option>
+            {/* Add more options as needed */}
+          </select>
+        </label>
+        <label>
+          Select number:
+          <select 
+            value={selectedNumber}
+            onChange={(e) => setSelectedNumber(e.target.value)}
+          >
+            <option value="">Select number</option>
+            {[...Array(10).keys()].map(num => (
+              <option key={num + 1} value={num + 1}>{num + 1}</option>
+            ))}
+          </select>
+        </label>
       </div>
-    </Router>
+      <HotelList 
+        searchLocation={searchLocation} 
+        locationType={selectedType} 
+        bedroomNumber={selectedNumber} 
+      />
+    </div>
+    
   );
 }
 
